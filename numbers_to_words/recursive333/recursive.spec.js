392d2ef204853c7ae9d5468927f66ec033e584fa
stevejpurves <stevejpurves@gmail.com>
2013-08-22 23:19:38 +0100

    green for 30

--- numbers_to_words/recursive333/recursive.spec.js
+++ numbers_to_words/recursive333/recursive.spec.js
@@ -3,7 +3,8 @@ function numbers_to_words( number ) {
 	var words = ["","one","two","three", "four", "five","six","seven","eight",
 	"nine","ten","eleven","twelve","thirteen","fourteen","fifteen","sizteen",
 	"seventeen","eighteen","nineteen","twenty"];
-	if (number > 20)
+	words[30] = "thirty";
+	if (words[number] === undefined)
 		return numbers_to_words(20) + " " + numbers_to_words(number-20);
 	return words[number];
 }
@@ -22,14 +23,16 @@ describe("when converting numbers to words", function(){
 		it("twenty", function(){
 			expect( numbers_to_words(20) ).toBe("twenty");
 		});
+
+		it("thirty", function(){
+			expect( numbers_to_words(30) ).toBe("thirty");
+		});
+
 	});
 	
 	describe("other numbers", function(){
 		it("twenty one", function(){
 			expect( numbers_to_words(21) ).toBe("twenty one");
-		});
-		it("thirty one", function(){
-			expect( numbers_to_words(31) ).toBe("thirty one");
 		});
 	});
 });
