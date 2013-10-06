611025a14df71103ca3937b3087fc59f9ecc0278
Steve Purves <stevejpurves@gmail.com>
2013-10-06 22:58:31 +0100

    refactor

--- numbers_to_words/tdd_solution/tdd_2.spec.js
+++ numbers_to_words/tdd_solution/tdd_2.spec.js
@@ -3,8 +3,8 @@
 function numbers_to_words(number) {
     if (number === null || number < 1)
         return "";
-    if (number > 1)
-        return "one hundred";
+    if (number >= 100)
+        return numbers_to_words(number / 100) + " hundred";
     return "one";
 }
 
