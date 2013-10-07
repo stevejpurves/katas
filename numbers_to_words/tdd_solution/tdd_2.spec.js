9215046dd19d9115eef1dc029c1802cc8453522b
Steve Purves <stevejpurves@gmail.com>
2013-10-07 10:25:11 +0100

    green for 1001 again

--- numbers_to_words/tdd_solution/tdd_2.spec.js
+++ numbers_to_words/tdd_solution/tdd_2.spec.js
@@ -12,8 +12,7 @@ function numbers_to_words(number) {
 
     var orders = [1000000, 1000, 100, 10, 1];
 
-    if (number < 20 )
-        return keywords[number];
+    if (number < 20 ) return keywords[number];
 
     for (var i = 0; orders[i] > 10; i++) {
         var words = "";
@@ -21,7 +20,7 @@ function numbers_to_words(number) {
         if (number_of_order >= 1) {
             words += numbers_to_words(number_of_order) + keywords[orders[i]];
             var remainder = (number - number_of_order*orders[i]);
-            if ( remainder > 0)
+            if ( remainder > 0 )
                 words += " and " + numbers_to_words(remainder);
             return words;
         }
@@ -41,56 +40,54 @@ describe("converting numbers to words", function() {
         expectNumberAsWords(-1, "");
     });
 
-    describe("for singular orders of magnitude", function(){
-        it("1", function(){
-            expectNumberAsWords(1,"one");
-        });
-
-        it("100", function(){
-            expectNumberAsWords(100, "one hundred");
-        });
-
-        it("1,000", function(){
-            expectNumberAsWords(1000, "one thousand");
-        });
-
-        it("10,000", function(){
-            expectNumberAsWords(10000, "ten thousand");
-        });
-
-        it("100,000", function(){
-            expectNumberAsWords(100000, "one hundred thousand");
-        });
-
-        it("1,000,000", function(){
-            expectNumberAsWords(1000000, "one million");
-        });
-
-        it("10,000,000", function(){
-            expectNumberAsWords(10000000, "ten million");
-        });
-
-        it("100,000,000", function(){
-            expectNumberAsWords(100000000, "one hundred million");
-        });
+    it("1", function(){
+        expectNumberAsWords(1,"one");
     });
 
-    describe("for numbers that contain a single word", function() {
-        it("2", function(){
-            expectNumberAsWords(2, "two");
-        });
-        it("19", function(){
-            expectNumberAsWords(19, "nineteen");
-        });
+    it("100", function(){
+        expectNumberAsWords(100, "one hundred");
     });
 
-    describe("numbers containing the word 'and'", function() {
-        it("101", function(){
-            expectNumberAsWords(101, "one hundred and one");
-        });
+    it("1,000", function(){
+        expectNumberAsWords(1000, "one thousand");
+    });
 
-        it("201", function(){
-            expectNumberAsWords(201, "two hundred and one");
-        });
+    it("10,000", function(){
+        expectNumberAsWords(10000, "ten thousand");
+    });
+
+    it("100,000", function(){
+        expectNumberAsWords(100000, "one hundred thousand");
+    });
+
+    it("1,000,000", function(){
+        expectNumberAsWords(1000000, "one million");
+    });
+
+    it("10,000,000", function(){
+        expectNumberAsWords(10000000, "ten million");
+    });
+
+    it("100,000,000", function(){
+        expectNumberAsWords(100000000, "one hundred million");
+    });
+
+    it("2", function(){
+        expectNumberAsWords(2, "two");
+    });
+    it("19", function(){
+        expectNumberAsWords(19, "nineteen");
+    });
+
+    it("101", function(){
+        expectNumberAsWords(101, "one hundred and one");
+    });
+
+    it("201", function(){
+        expectNumberAsWords(201, "two hundred and one");
+    });
+
+    it("1001", function(){
+        expectNumberAsWords(1001, "one thousand and one");
     });
 });
